d8c25316f9d1cb0a699d1c3e3fb216727ddd5c79
Nikola Limpet <2023465lim@aupp.edu.kh>
2024-11-11 10:04:24 +0700

    add tabbar and styling

--- new/taskly2/app/+not-found.tsx
+++ new/taskly2/app/+not-found.tsx
@@ -1,32 +1,35 @@
+import { View, StyleSheet, Text } from 'react-native';
 import { Link, Stack } from 'expo-router';
-import { StyleSheet } from 'react-native';
-
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
 
 export default function NotFoundScreen() {
-  return (
-    <>
-      <Stack.Screen options={{ title: 'Oops!' }} />
-      <ThemedView style={styles.container}>
-        <ThemedText type="title">This screen doesn't exist.</ThemedText>
-        <Link href="/" style={styles.link}>
-          <ThemedText type="link">Go to home screen!</ThemedText>
-        </Link>
-      </ThemedView>
-    </>
-  );
+    return (
+        <>
+            <Stack.Screen options={{ title: 'Not Found' }} />
+            <View style={styles.container}>
+                <Link href="/" style={styles.button}>
+                    Go to Home
+                </Link>
+            </View>
+        </>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-  },
-  link: {
-    marginTop: 15,
-    paddingVertical: 15,
-  },
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#25292e',
+    },
+    text: {
+        color: '#fff',
+    },
+    button: {
+        color: '#fff',
+        fontSize: 20,
+        padding: 10,
+        backgroundColor: '#007bff',
+        borderRadius: 5,
+        marginTop: 10,
+    },
 });
